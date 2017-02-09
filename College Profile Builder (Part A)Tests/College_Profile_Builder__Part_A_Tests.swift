787b5b9e8a47106279f8f3e9608d00b82b4568a8
Lesslie Garcia <student@lgarcia1.local>
2017-02-09 11:16:49 -0600

    Initial Commit

---
+++ College Profile Builder (Part A)Tests/College_Profile_Builder__Part_A_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  College_Profile_Builder__Part_A_Tests.swift
+//  College Profile Builder (Part A)Tests
+//
+//  Created by Lesslie Garcia on 2/9/17.
+//  Copyright © 2017 Lesslie Garcia. All rights reserved.
+//
+
+import XCTest
+@testable import College_Profile_Builder__Part_A_
+
+class College_Profile_Builder__Part_A_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
