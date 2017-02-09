2dd44e9c9b09938325326b9a0bddc1ef8f773278
Lesslie Garcia <student@lgarcia1.local>
2017-02-09 11:33:08 -0600

    Created Custom Class

---
+++ College.swift
@@ -0,0 +1,29 @@
+//
+//  College.swift
+//  College Profile Builder (Part A)
+//
+//  Created by Lesslie Garcia on 2/9/17.
+//  Copyright © 2017 Lesslie Garcia. All rights reserved.
+//
+
+import UIKit
+
+class College: NSObject {
+    var collegeName = String()
+    var location = String()
+    var enrollment = String()
+    var image = Data()
+    
+    
+    // parameters -- what is passed into the function //
+    convenience init(collegeName: String,location: String, enrollment: String, image: Data) {
+        self.init()
+        self.collegeName = collegeName
+        self.location = location
+        self.enrollment = enrollment
+        self.image = image
+   
+    }
+    
+
+}
